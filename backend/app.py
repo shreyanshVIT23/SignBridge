ecd6ed7604c3f6100944632db5bc57417dfeae83
shreyanshVIT23 <179868648+shreyanshVIT23@users.noreply.github.com>
2025-06-16 22:39:01 +0530

    Deploy setup

--- backend/app.py
+++ backend/app.py
@@ -27,7 +27,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Your frontend URL
+    allow_origins=["*"],  # Your frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
